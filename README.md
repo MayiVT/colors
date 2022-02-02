ddffe257ad10d7ca45d740c4d7c5a06c45f9e61f
MayiVT <78113457+MayiVT@users.noreply.github.com>
2022-02-02 20:58:05 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# colors
-colors npm library to readd Strings Color to Discord.js@v14
+# Colors!
+
+## Installation
+
+```bash
+npm install colors-discord
+```
+
+## Usage
+
+```javascript
+const colors = require('colors-discord');
+
+const embed = new Embed() //these depends your discord.js version.
+      .setTitle("Test")
+      .setColor(colors("red"));
+
+message.reply({embeds: [embed]})
+```
+
+## Colors
+
+- default
+- aqua
+- dark_aqua
+- green
+- dark_green
+- blue
+- dark_blue
+- purple
+- dark_purple
+- luminous_vivid_pink
+- dark_vivid_pink
+- gold
+- dark_gold
+- orange
+- dark_orange
+- red
+- dark_red
+- grey
+- dark_grey
+- darker_grey
+- light_grey
+- navy
+- dark_navy
+- yellow
+- white
+- blurple
+- greyple
+- dark_but_not_black
+- not_quite_black
+- green
+- yellow2
+- fuschia
+- red2
+- black
+- unnamed_role_color_1
+- unnamed_role_color_2
+
+## ToDo
+
+- Random Color
+- HeX Integration
+- Add More Colors
+- Docs Guide
+- Easy Integration
+
+## Contribution
+
+[HERE](https://github.com/MayiVT/colors-discord/)
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ index.js
@@ -0,0 +1,120 @@
+function colors(string) {
+    if(!string) return console.log("You need to define a color!");
+    string = string.toLowerCase();
+
+    //By name
+    if(string == "default") {
+        return string = parseInt("0");
+    }
+    if(string == "aqua") {
+        return string = parseInt("1752220");
+    }
+    if(string == "dark_aqua") {
+        return string = parseInt("1146986");
+    }
+    if(string == "green") {
+        return string = parseInt("3066993");
+    }
+    if(string == "dark_green") {
+        return string = parseInt("2067276");
+    }
+    if(string == "blue") {
+        return string = parseInt("3447003");
+    }
+    if(string == "dark_blue") {
+        return string = parseInt("2123412");
+    }
+    if(string == "purple") {
+        return string = parseInt("10181046");
+    }
+    if(string == "dark_purple") {
+        return string = parseInt("7419530");
+    }
+    if(string == "luminous_vivid_pink") {
+        return string = parseInt("15277667");
+    }
+    if(string == "dark_vivid_pink") {
+        return string = parseInt("11342935");
+    }
+    if(string == "gold") {
+        return string = parseInt("15844367");
+    }
+    if(string == "dark_gold") {
+        return string = parseInt("12745742");
+    }
+    if(string == "orange") {
+        return string = parseInt("15105570");
+    }
+    if(string == "dark_orange") {
+        return string = parseInt("11027200");
+    }
+    if(string == "red") {
+        return string = parseInt("15158332");
+    }
+    if(string == "dark_red") {
+        return string = parseInt("10038562");
+    }
+    if(string == "grey") {
+        return string = parseInt("9807270");
+    }
+    if(string == "dark_grey") {
+        return string = parseInt("9936031");
+    }
+    if(string == "darker_grey") {
+        return string = parseInt("8359053");
+    }
+    if(string == "light_grey") {
+        return string = parseInt("12370112");
+    }
+    if(string == "navy") {
+        return string = parseInt("3426654");
+    }
+    if(string == "dark_navy") {
+        return string = parseInt("2899536");
+    }
+    if(string == "yellow") {
+        return string = parseInt("16776960");
+    }
+    if(string == "white") {
+        return string = parseInt("16777215");
+    }
+    if(string == "blurple") {
+        return string = parseInt("5793266");
+    }
+    if(string == "greyple") {
+        return string = parseInt("10070709");
+    }
+    if(string == "dark_but_not_black") {
+        return string = parseInt("2895667");
+    }
+    if(string == "not_quite_black") {
+        return string = parseInt("2303786");
+    }
+    if(string == "green") {
+        return string = parseInt("5763719");
+    }
+    if(string == "yellow2") {
+        return string = parseInt("16705372");
+    }
+    if(string == "fuschia") {
+        return string = parseInt("15418782");
+    }
+    if(string == "red2") {
+        return string = parseInt("15548997");
+    }
+    if(string == "black") {
+        return string = parseInt("2303786");
+    }
+    if(string == "unnamed_role_color_1") {
+        return string = parseInt("0");
+    }
+    if(string == "unnamed_role_color_2") {
+        return string = parseInt("0");
+    }
+}
+//modules exports
+module.exports = colors;
+
+//By iMayiVT
+//Discord: 🌸₊˚๑ iMayiVT ✧⁺ 🍡#4001
+//Do not remove credits if forked.
